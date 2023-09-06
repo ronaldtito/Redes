07058c0d247a918d23fe186087f557a580829a9a
ronaldtito <ronaldtito@users.noreply.github.com>
2023-09-06 07:44:08 -0500

    Add files via upload

---
+++ socket/ccr/01/client.c
@@ -0,0 +1,69 @@
+ /* Client code in C */
+ 
+  #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netinet/in.h>
+  #include <arpa/inet.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <unistd.h>
+ 
+  int main(void)
+  {
+    struct sockaddr_in stSockAddr;
+    int Res;
+    int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int n;
+ 
+    if (-1 == SocketFD)
+    {
+      perror("cannot create socket");
+      exit(EXIT_FAILURE);
+    }
+ 
+    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+ 
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(1100);
+    Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);
+ 
+    if (0 > Res)
+    {
+      perror("error: first parameter is not a valid address family");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+    else if (0 == Res)
+    {
+      perror("char string (second parameter does not contain valid ipaddress");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+ 
+    if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+    {
+      perror("connect failed");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+    char data[256];
+    char buffer[256];
+
+    while(1){
+      printf("Data to be send: ");
+      fgets(data,256,stdin);
+      if(strcmp(data, "END\n") == 0){
+        write(SocketFD,"END",3);
+        break;
+      }
+      write(SocketFD,data,strlen(data));
+      
+    }
+    bzero(buffer,256);
+    n = read(SocketFD,buffer,255);
+    shutdown(SocketFD, SHUT_RDWR);
+ 
+    close(SocketFD);
+    return 0;
+  }
